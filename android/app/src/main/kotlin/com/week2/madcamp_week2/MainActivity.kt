20cb25c2ccb44dba26afea4a0fe74c0e20340332
sunnygeem <marguerite27@hanyang.ac.kr>
2024-01-05 11:28:56 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/week2/madcamp_week2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.week2.madcamp_week2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Week2'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: IndexedStack(
+          index: _currentIndex,
+          children: const[
+            Text('tab1'),
+            Text('tab2'),
+            Text('tab3'),
+            Text('tab4'),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (value) {
+          setState((){
+            _currentIndex = value;
+          });
+        },
+        items: const[
+          BottomNavigationBarItem(
+              label: 'Walk',
+              icon: Icon(Icons.directions_walk),
+          ),
+          BottomNavigationBarItem(
+            label: 'View',
+            icon: Icon(Icons.remove_red_eye),
+          ),
+          BottomNavigationBarItem(
+            label: 'Mate',
+            icon: Icon(Icons.people_alt),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+    );
+  }
+}
