bb8ba9f51d76181b3f913235360cf61f9ae3a0b5
sunnygeem <marguerite27@hanyang.ac.kr>
2024-01-06 10:32:55 +0900

    rollback

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 void main() {
-  KakaoSdk.init(nativeAppKey: 'f6e96e5eb182ce45ba336e6b156a718e',);
   runApp(const MyApp());
 }
 
@@ -18,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: KakaoLoginPage(),
+      home: const MyHomePage(title: 'Week2'),
     );
   }
 }
@@ -62,8 +60,8 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         items: const[
           BottomNavigationBarItem(
-              label: 'Walk',
-              icon: Icon(Icons.directions_walk),
+            label: 'Walk',
+            icon: Icon(Icons.directions_walk),
           ),
           BottomNavigationBarItem(
             label: 'View',
@@ -78,52 +76,6 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: Icon(Icons.person),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class KakaoLoginPage extends StatefulWidget {
-  @override
-  _KakaoLoginPageState createState() => _KakaoLoginPageState();
-}
-
-class _KakaoLoginPageState extends State<KakaoLoginPage> {
-  Future<void> _loginButtonPressed() async {
-    try {
-      await UserApi.instance.loginWithKakaoAccount();
-      print('카카오계정으로 로그인 성공');
-      Navigator.push(context, MaterialPageRoute(
-          builder: (context) => MyHomePage(title: 'week2'),
-      ),);
-    } catch (error) {
-      print('카카오계정으로 로그인 실패 $error');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        minimum: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                onPressed: _loginButtonPressed,
-                child: Text(
-                  '카카오 로그인',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black87,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
