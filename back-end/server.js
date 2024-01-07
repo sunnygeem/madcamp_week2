e75bad6ba70b2afc4e526f9b0421e445478e0414
sunnygeem <marguerite27@hanyang.ac.kr>
2024-01-07 11:23:41 +0000

    db connected

--- back-end/server.js
+++ back-end/server.js
@@ -1,76 +1,19 @@
-const express = require('express');
-const path = require('path');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-
-
+import mysql from 'mysql2';
 
 var con = mysql.createConnection({
-    host: '127.0.0.1',
+    host: '15.164.95.87',
     user: 'root',
-    password: '0220',
-    port: 3309,
-    database: 'week2'
-
+    database: 'footprint',
+	password: '1234abcd',
   });
 
 
-
 con.connect(function(err){
-  if (err) throw err;
+  if (err){
+    console.log('error: ', err);
+  }
+	else{
   console.log('Connected');
+	}
 })
-
-
- const app = express();
- app.use(express.json());
-
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({extended:true}));
-
-// app.use("/",function(req,res,next) {
-// 	res.writeHead("200", {"Content-Type":"text/html;charset=utf-8"});
-// 	res.end("<h1>Express 서버에서 " + req.user + " 응답한 결과입니다</h1>");
-// 	// res.write로 길게 안쓰고 res.end에 간결하게 보내줌
-// });
-
- app.listen(8000, function () {
-   console.log('listening on 8000')
- });
-
-//
-app.get('/get', function(req, res){
-    var sql = 'select * from account';
-    con.query(sql, function(err, id, fields){
-      var user_id = req.params.id;
-      if(id){
-        var sql='select * from account'
-        con.query(sql, function(err, result, fields){
-          if(err){
-            console.log(err);
-          }else{
-            res.json(id);
-            console.log('user:', user_id);
-            console.log('user:', fields);
-          }
-        })
-      }
-    })
-  })
-
-app.post("/sendAcc", function(req,res){
-        const body = req.body;
-        var sql = 'INSERT INTO account (account_email, account_name) VALUES (?, ?)';
-        con.query(sql, [body.account_email, body.account_name], function(error, result, field){
-            if(error){
-            console.error(error);
-            }
-        })
-      })
-
-app.post("/send", function(req,res){
-    const body = req.body;
-    var sql = 'INSERT INTO table1 (id, name) VALUES (?, ?)';
-    con.query(sql, [body.id, body.name], function(error, result, field){})
-  })
 
