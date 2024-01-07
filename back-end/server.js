2b64b18f126d4f63005bd5093df85cccfea8764d
sunnygeem <marguerite27@hanyang.ac.kr>
2024-01-07 19:35:41 +0000

    new_server

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 var con = mysql.createConnection({
     host: '15.164.95.87',
@@ -19,6 +20,15 @@ con.connect(function(err){
 })
 
 const app = express();
+
+app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended:true}));
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', function(req, res){
+res.send('hi');
+})
 
 app.get('/get', function(req, res){
     var sql = 'select * from user';
@@ -47,3 +57,5 @@ app.post("/sendUserInfo", function(req,res){
             }
         })
       })
+
+app.listen(PORT, function(){console.log('now on port 5000')})
